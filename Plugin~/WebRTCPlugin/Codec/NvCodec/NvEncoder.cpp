bfa210045925d3fba67a6ed61571b50c8dcbc2e6
Tomasz Slanda <tkslan@gmail.com>
2020-06-19 16:18:09 +0200

    VS2019 project upgrade

--- Plugin~/WebRTCPlugin/Codec/NvCodec/NvEncoder.cpp
+++ Plugin~/WebRTCPlugin/Codec/NvCodec/NvEncoder.cpp
@@ -27,7 +27,7 @@ namespace webrtc
         LogPrint(StringFormat("width is %d, height is %d", width, height).c_str());
         checkf(width > 0 && height > 0, "Invalid width or height!");
 
-        m_bitrateAdjuster = std::make_unique<webrtc::BitrateAdjuster>(0.5f, 0.95f);
+       // m_bitrateAdjuster = std::make_unique<webrtc::BitrateAdjuster>(0.5f, 0.95f);
     }
 
     void NvEncoder::InitV()
@@ -85,7 +85,7 @@ namespace webrtc
         std::memcpy(&nvEncConfig, &presetConfig.presetCfg, sizeof(NV_ENC_CONFIG));
         nvEncConfig.profileGUID = NV_ENC_H264_PROFILE_BASELINE_GUID;
         nvEncConfig.gopLength = nvEncInitializeParams.frameRateNum;
-        nvEncConfig.rcParams.averageBitRate = m_bitrateAdjuster->GetAdjustedBitrateBps();
+        nvEncConfig.rcParams.averageBitRate = 10000000;
         nvEncConfig.encodeCodecConfig.h264Config.idrPeriod = nvEncConfig.gopLength;
 
         nvEncConfig.encodeCodecConfig.h264Config.sliceMode = 0;
